20de02ff9b4dd18ebdaf3c8a84e839c8df2b52a9
Rafal Pijet <stronglopez@wp.pl>
2019-03-29 21:46:00 +0100

    Adding recommendation from 25.6 task

--- server.js
+++ server.js
@@ -2,51 +2,38 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', './myViews');
+app.use(express.static('images'));
 
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
+app.get('/login', function (req, res) {
+    res.render('login')
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
-
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function (req, res) {
-    res.render('first-template');
-});
-
-app.get('/dynamic-view', function (req, res) {
-    res.render('dynamic', {
-        name: "Moja dynamiczna strona",
-        url: "http://www.google.pl"
+app.get('/main', function (req, res) {
+    const response = {
+        user_email: req.query.email,
+        user_password: req.query.password
+    };
+    
+    res.render('main', {
+        user_name: "Wladimir Putin",
+        user_email: response.user_email,
+        first: "Jaroslaw Kaczynski",
+        thirdh: "Tadeusz Rydzyk"
     })
 });
 
-app.get("/dynamic-user", function (req, res) {
-    res.render("dynamic-user", {
-       user: {
-           name: "David", age: "51"
-       }
+app.get('/auth/google', function (req, res) {
+    res.render('main', {
+        user_name: "Wladimir Putin",
+        user_email: "putin@russiandevil.ru",
+        first: "Jaroslaw Kaczynski",
+        thirdh: "Tadeusz Rydzyk"
     })
-});
-
-app.get("/content", function (req, res) {
-    res.render("content")
 });
 
 app.use(function (req, res) {
     res.status(404).send('Sorka, ale chyba dałeś ciała wysyłyjąc tego requesta ;)');
 });
 
-const server = app.listen(3000, 'localhost', function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
+const server = app.listen(3000);
