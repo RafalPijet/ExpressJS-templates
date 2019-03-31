69b74af45fb71250bda510666d94192ceabfb835
Rafal Pijet <stronglopez@wp.pl>
2019-03-31 14:35:11 +0200

    Adding recommendation from 25.7 task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '124545713549-scvvlr193f5qd1gsrkbc45bj3q9e74d1.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '5cznyF0Sg90ynDvrqg0Bgpib',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+var googleProfile = {};
 
 app.set('view engine', 'pug');
 app.set('views', './myViews');
 app.use(express.static('images'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+            image: profile.photos[0].value
+        };
+        cb(null, profile);
+    }
+));
+
+function isEmpty(obj) {
+    for (var key in obj) {
+        
+        if (obj.hasOwnProperty(key))
+            return false;    
+    }
+    return true
+};
 
 app.get('/login', function (req, res) {
-    res.render('login')
+    res.render('login', {user: req.user})
 });
 
 app.get('/main', function (req, res) {
@@ -15,22 +56,31 @@ app.get('/main', function (req, res) {
         user_password: req.query.password
     };
     
+    let check = isEmpty(googleProfile);
+
     res.render('main', {
         user_name: "Wladimir Putin",
         user_email: response.user_email,
         first: "Jaroslaw Kaczynski",
-        thirdh: "Tadeusz Rydzyk"
-    })
+        thirdh: "Tadeusz Rydzyk",
+        user: googleProfile,
+        check: check
+    });
+    console.log(check);
+    googleProfile = {};
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('main', {
-        user_name: "Wladimir Putin",
-        user_email: "putin@russiandevil.ru",
-        first: "Jaroslaw Kaczynski",
-        thirdh: "Tadeusz Rydzyk"
-    })
-});
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    }));
+
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/main',
+        failureRedirect: '/'
+    }));
 
 app.use(function (req, res) {
     res.status(404).send('Sorka, ale chyba dałeś ciała wysyłyjąc tego requesta ;)');
